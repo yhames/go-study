5e8cfc36407ab0210e4e076bc80dafd1e17bb71b
yhames <wjddn494@naver.com>
2025-06-10 00:17:05 +0900

    Implement basic HTTP server with hello world handler

--- curd-server/init/main.go
+++ curd-server/init/main.go
@@ -1,4 +1,24 @@
 package main
 
+import (
+	"fmt"
+	"net/http"
+)
+
 func main() {
+	http.HandleFunc("/", helloHandler)
+
+	err := http.ListenAndServe(":8080", nil)
+	if err != nil {
+		fmt.Println("Error starting server:", err)
+		return
+	}
+}
+
+func helloHandler(w http.ResponseWriter, r *http.Request) {
+	_, err := w.Write([]byte("Hello World!"))
+	if err != nil {
+		fmt.Println("Error writing response:", err)
+		return
+	}
 }
